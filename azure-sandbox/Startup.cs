12129ac9d7433319dd45b0751d7f71b3b6aef57f
Johnny Oshika <johnnyoshika@gmail.com>
2014-03-03 08:45:06 -0800

    Loading project from template.

---
+++ azure-sandbox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(azure_sandbox.Startup))]
+namespace azure_sandbox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
